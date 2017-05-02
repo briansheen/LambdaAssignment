bf34e00b70c23dfc33f5576f4e3192e38b514b9b
Brian Sheen <bsheen@gmail.com>
2017-05-02 17:34:55 -0500

    initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+    RoboCallTest01.main(args);
+    RoboCallTest02.main(args);
+    RoboCallTest03.main(args);
+    RoboCallTest04.main(args);
+    
+  }
+}
---
+++ src/com/example/Predicate.java
@@ -0,0 +1,4 @@
+package com.example;
+public interface Predicate<T> {
+  public boolean test(T t);
+}
